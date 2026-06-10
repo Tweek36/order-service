cd3f123fe5b1da220a0069028080f4bca5dcbcbf
tweek36 <sowakov.yura@gmail.com>
2026-06-10 20:32:12 +0300

    fix: simplify NEW status notification message to 'Order created'

--- app/application/use_cases/create_order.py
+++ app/application/use_cases/create_order.py
@@ -96,7 +96,7 @@ class CreateOrderUseCase:
         # Отправляем уведомление о создании заказа
         try:
             await self.notifications_client.send_notification(
-                message="Your order created",
+                message="Order created",
                 reference_id=order.id,
                 idempotency_key=f"order-created-{order.id}",
             )
